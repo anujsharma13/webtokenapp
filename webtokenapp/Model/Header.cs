509dbdba88290f8f2f6fe7aff1fb479e0ec8e5b0
Admin <Admin@DESKTOP-D03479E>
2021-09-08 13:02:31 +0530

    Add project files.

---
+++ webtokenapp/Model/Header.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace webtokenapp.Model
+{
+    public class Header
+    {
+        public string alg { get; set; } = "HS256";
+        public string typ { get; set; } = "JWT";
+
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public string ToBase64String()
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(ToJson()));
+        }
+    }
+}
---
+++ webtokenapp/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace webtokenapp.Model
+{
+    public class Person
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string ToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public string ToBase64String()
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(ToJson()));
+        }
+    }
+}
